8752ab9a6251285a34b11daa69aabf9ca9ac2c33
pedrogilsenarego <pedrogilsenarego@gmail.com>
2021-08-03 23:49:44 +0100

    a

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from "./NavBar/NavBar"
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Navbar/>
         <img src={logo} className="App-logo" alt="logo" />
         Hello Baby Amote Muito! ;)
       </header>
---
+++ src/NavBar/NavBar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import Button from "@material-ui/core/Button"
+import  Typography  from '@material-ui/core/Typography'
+import AppBar from '@material-ui/core/AppBar'
+import ToolBar from '@material-ui/core/ToolBar'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
+
+const Navbar = () => {
+    return (
+        <AppBar color="secondary">
+                    <ToolBar>
+                    <IconButton>
+                    <MenuIcon/>
+                </IconButton>
+                        <Typography variant="h6">
+                            MUI Theming
+                        </Typography>
+                        <Button>
+                            Login
+                        </Button>
+                    </ToolBar>
+                </AppBar>
+    )
+}
+
+export default Navbar
