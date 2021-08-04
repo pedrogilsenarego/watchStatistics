a35716867ce9c9df5c0b77f48fc4cba07cbbcf7d
pedrogilsenarego <pedrogilsenarego@gmail.com>
2021-08-04 18:30:37 +0100

    a

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,29 @@
-import React, {useState, useEffect} from 'react';
-import Axios from "axios";
+import React from 'react';
+
 
 import logo from './logo.svg';
 import './App.css';
-import Navbar from "./NavBar/NavBar"
+import Navbar from "./components/NavBar/NavBar"
+import Student from './components/Student';
  
 
 
 function App() {
 
-  const [userName, setUserName] = useState("")
-  const [email, setEmail] = useState("")
-
-  const submitUser = () => {
-    Axios.post("http://localhost:3001/api/insert", {
-      userName: userName, 
-      email: email,
-    }).then(()=>{
-        alert("sucessfuk insert");
-    });
-
-  };
-
+  
+     
   return (
     <div className="App">
       <header className="App-header">
         <Navbar/>
+        
         <img src={logo} className="App-logo" alt="logo" />
-        <label>Name</label>
-        <input type="text" name="userName" onChange={(e)=>{
-            setUserName(e.target.value)
-        }}/>
-        <label>Email</label>
-        <input type="text" name="email" onChange={(e)=>{
-          setEmail(e.target.value)
-        }}/>
-        <button onClick={submitUser}>Submit</button>
+        <Student/>
+        
       </header>
     </div>
   );
 }
 
 export default App;
+
