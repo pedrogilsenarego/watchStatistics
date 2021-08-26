a5a71f83e14c266930e2c38dd691d2e8261d9a2e
pedrogilsenarego <pedrogilsenarego@gmail.com>
2021-08-26 19:33:49 +0100

    a

--- expressBackEnd/database.js
+++ expressBackEnd/database.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 const connection = mysql.createConnection({
 	host: process.env.MYSQL_HOST || "localhost",
 	user: process.env.MYSQL_USER || "root",
-	password: process.env.MYSQL_PASSWORD || "1974Theyseeeverything",
+	password: process.env.MYSQL_PASSWORD || "",
 	database: process.env.MYSQL_DATABASE || "watchstatistics"
 });
 
