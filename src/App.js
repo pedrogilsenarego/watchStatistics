feb1261bec6baf2276b29ff36d8efeb94c4dbabe
pedrogilsenarego <pedrogilsenarego@gmail.com>
2021-08-30 18:34:02 +0100

    a

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
 import "./default.scss";
+import { auth } from "./firebase/utils";
 
 //pages
 import Homepage from "./pages/Homepage";
@@ -10,40 +11,60 @@ import Login from "./pages/Login";
 // layouts
 import HomepageLayout from "./layouts/HomepageLayout";
 
-function App() {
-	return (
-		<div className="App">
-			<Switch>
-				<Route
-					exact
-					path="/"
-					render={() => (
-						<HomepageLayout>
-							<Homepage />
-						</HomepageLayout>
-					)}
-				/>
-				<Route
-					exact
-					path="/registration"
-					render={() => (
-						<HomepageLayout>
-							<Registration />
-						</HomepageLayout>
-					)}
-				/>
-				<Route
-					exact
-					path="/login"
-					render={() => (
-						<HomepageLayout>
-							<Login />
-						</HomepageLayout>
-					)}
-				/>
-			</Switch>
-		</div>
-	);
+const initialState = {
+	currentUser: null
+};
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			...initialState
+		};
+	}
+
+	authListener = null;
+
+	componentDidMount() {
+		this.authListener = auth.onAuthStateChanged();
+	}
+	componentWillUnmount() {}
+
+	render() {
+		return (
+			<div className="App">
+				<Switch>
+					<Route
+						exact
+						path="/"
+						render={() => (
+							<HomepageLayout>
+								<Homepage />
+							</HomepageLayout>
+						)}
+					/>
+					<Route
+						exact
+						path="/registration"
+						render={() => (
+							<HomepageLayout>
+								<Registration />
+							</HomepageLayout>
+						)}
+					/>
+					<Route
+						exact
+						path="/login"
+						render={() => (
+							<HomepageLayout>
+								<Login />
+							</HomepageLayout>
+						)}
+					/>
+				</Switch>
+			</div>
+		);
+	}
 }
 
 export default App;
