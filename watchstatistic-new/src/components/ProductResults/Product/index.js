8f324bb557a003925ad2acaafe7cabeb9814a15a
pedrogilsenarego <pedrogilsenarego@gmail.com>
2021-09-16 09:11:19 +0100

    main

--- watchstatistic-new/src/components/ProductResults/Product/index.js
+++ watchstatistic-new/src/components/ProductResults/Product/index.js
@@ -8,6 +8,19 @@ const useStyles = makeStyles((theme) => ({
 	media: {
 		height: 200
 	},
+	media2: {
+		height: 200,
+		"&:hover": {
+			transform: "scale(1.2)",
+			transition: "all 3s ease 0s"
+		}
+	},
+	imageWrapper: {
+		overflow: "hidden",
+		height: 200,
+		display: "inline - block"
+	},
+
 	text: {
 		color: "#FFFFFF",
 		fontSize: "20px",
@@ -40,69 +53,70 @@ const Product = (product) => {
 					className={classes.media}
 					image={productThumbnail}
 					key={1}
-					title="hello"
 					onMouseEnter={() => setOnMouse(false)}
 				/>
 			)}
 			{!onMouse && (
-				<CardMedia
-					className={classes.media}
-					image={productThumbnail}
-					key={1}
-					style={{
-						position: "relative",
-
-						cursor: "pointer"
-					}}
-					onMouseLeave={() => setOnMouse(true)}
-					onClick={() => history.push(`/product/${documentID}`)}
-				>
-					{" "}
-					<Box
-						height={"100%"}
-						bgcolor="#040406D1"
+				<div className={classes.imageWrapper}>
+					<CardMedia
+						className={classes.media2}
+						image={productThumbnail}
+						key={1}
 						style={{
 							position: "relative",
 
 							cursor: "pointer"
 						}}
+						onMouseLeave={() => setOnMouse(true)}
+						onClick={() => history.push(`/product/${documentID}`)}
 					>
-						<Typography
+						{" "}
+						<Box
+							height={"100%"}
+							bgcolor="#040406B3"
 							style={{
-								cursor: "pointer",
-								paddingTop: "60px"
-							}}
-							key={2}
-							className={classes.text}
-							onMouseEnter={() => setOnMouse(false)}
-							onClick={() => history.push(`/product/${documentID}`)}
-						>
-							{productBrand} - {productName}
-						</Typography>
-						<Typography
-							style={{
+								position: "relative",
+
 								cursor: "pointer"
 							}}
-							key={3}
-							className={classes.text}
-							onMouseEnter={() => setOnMouse(false)}
-							onClick={() => history.push(`/product/${documentID}`)}
 						>
-							Score: {avgTotal}
-						</Typography>
-						<Typography
-							style={{
-								cursor: "pointer"
-							}}
-							key={4}
-							className={classes.text}
-							onMouseEnter={() => setOnMouse(false)}
-							onClick={() => history.push(`/product/${documentID}`)}
-						>
-							Votes: {numberVotesNotOwn + numberVotesOwn}
-						</Typography>
-					</Box>
-				</CardMedia>
+							<Typography
+								style={{
+									cursor: "pointer",
+									paddingTop: "60px"
+								}}
+								key={2}
+								className={classes.text}
+								onMouseEnter={() => setOnMouse(false)}
+								onClick={() => history.push(`/product/${documentID}`)}
+							>
+								{productBrand} - {productName}
+							</Typography>
+							<Typography
+								style={{
+									cursor: "pointer"
+								}}
+								key={3}
+								className={classes.text}
+								onMouseEnter={() => setOnMouse(false)}
+								onClick={() => history.push(`/product/${documentID}`)}
+							>
+								Score: {avgTotal}
+							</Typography>
+							<Typography
+								style={{
+									cursor: "pointer"
+								}}
+								key={4}
+								className={classes.text}
+								onMouseEnter={() => setOnMouse(false)}
+								onClick={() => history.push(`/product/${documentID}`)}
+							>
+								Votes: {numberVotesNotOwn + numberVotesOwn}
+							</Typography>
+						</Box>
+					</CardMedia>
+				</div>
 			)}
 		</Box>
 	);
--- watchstatistic-new/src/redux/Products/products.helpers.js
+++ watchstatistic-new/src/redux/Products/products.helpers.js
@@ -21,7 +21,7 @@ export const handleFetchProducts = ({
 	persistProducts = []
 }) => {
 	return new Promise((resolve, reject) => {
-		const pageSize = 6;
+		const pageSize = 8;
 
 		let ref = firestore
 			.collection("products")
