e53a3508849dec26eed3c0837a5ff1dd2f0a0e39
pedrogilsenarego <pedrogilsenarego@gmail.com>
2021-10-04 18:12:01 +0100

    a

--- watchstatistic-new/src/components/watchstatisticsAddWatch/index.js
+++ watchstatistic-new/src/components/watchstatisticsAddWatch/index.js
@@ -3,9 +3,9 @@ import { useDispatch } from "react-redux";
 import { addProductStart } from "../../redux/Products/products.actions";
 import { Form, Formik } from "formik";
 import * as Yup from "yup";
-import { Grid } from "@material-ui/core";
+import { Grid, Typography } from "@material-ui/core";
 import watchTypes from "./../../assets/data/watchTypes2.json";
-import watchBrands from "./../../assets/data/watchBrands.json";
+import watchBrands from "./../../assets/data/watchBrands2.json";
 
 import TextField from "../forms/InputMUI";
 import ButtonMUI from "../forms/ButtonMUI";
@@ -13,15 +13,19 @@ import Multiline from "../forms/MultiLineMUI";
 import Select from "../forms/SelectMUIFormik";
 
 const INITIAL_FORM_STATE = {
-	categories: "",
+	productCategory: "",
+	productBrand: "",
 	productName: "",
 	productThumbnail: "",
 	productBackground: "",
-	productDesc: ""
+	productDesc: "",
+	additionalDataTitle: "",
+	additionalDataLink: ""
 };
 
 const FORM_VALIDATION = Yup.object().shape({
-	categories: Yup.string().required("Required"),
+	productCategory: Yup.string().required("Required"),
+	productBrand: Yup.string().required("Required"),
 	productName: Yup.string().required("Required"),
 	productThumbnail: Yup.string()
 		.matches(
@@ -35,7 +39,14 @@ const FORM_VALIDATION = Yup.object().shape({
 			"Enter a valid URL!"
 		)
 		.required("Please enter Image URL adress"),
-	productDesc: Yup.string().required("Required")
+	productDesc: Yup.string().required("Required"),
+	additionalDataTitle: Yup.string().required("Required"),
+	additionalDataLink: Yup.string()
+		.matches(
+			/((https?):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/,
+			"Enter a valid URL!"
+		)
+		.required("Please enter Image URL adress")
 });
 
 const AddWatchForm = () => {
@@ -46,18 +57,24 @@ const AddWatchForm = () => {
 			productName,
 			productThumbnail,
 			productBackground,
+			productBrand,
 			productDesc,
+			additionalDataTitle,
+			additionalDataLink,
 			productCategory
 		} = e;
 
 		dispatch(
 			addProductStart({
 				productCategory,
-				productBrand: "rolex",
+				productBrand,
 				productName,
 				productBackground,
 				productThumbnail: [productThumbnail],
 				productDesc,
+				additionalData: [
+					{ title: additionalDataTitle, link: additionalDataLink }
+				],
 				avgTotal: 0,
 				numberVotesNotOwn: 0,
 				numberVotesOwn: 0,
@@ -85,9 +102,16 @@ const AddWatchForm = () => {
 						<Grid container>
 							<Grid item xs={12}>
 								<Select
-									name="categories"
+									name="productCategory"
 									label="Categories"
 									options={watchTypes}
+								/>
+							</Grid>
+							<Grid item xs={12}>
+								<Select
+									name="productBrand"
+									label="Brands"
+									options={watchBrands}
 								/>
 							</Grid>
 							<Grid item xs={12}>
@@ -107,6 +131,15 @@ const AddWatchForm = () => {
 									name="productDesc"
 									label="Description from Watch"
 								></Multiline>
+							</Grid>
+							<Grid item xs={12}>
+								<Typography>References</Typography>
+							</Grid>
+							<Grid item xs={12}>
+								<TextField name="additionalDataTitle" label="Title"></TextField>
+							</Grid>
+							<Grid item xs={12}>
+								<TextField name="additionalDataLink" label="Link"></TextField>
 							</Grid>
 
 							<Grid item xs={12} style={{ paddingTop: "20px" }}>
