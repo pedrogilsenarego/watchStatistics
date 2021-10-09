3d1eb73504ec13ac315f2e45ba42425e2a8d619d
pedrogilsenarego <pedrogilsenarego@gmail.com>
2021-10-09 02:02:27 +0100

    a

--- watchstatistic-new/src/firebase/utils.js
+++ watchstatistic-new/src/firebase/utils.js
@@ -1,7 +1,16 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/auth";
-import { firebaseConfig } from "../Secrets/firebase-config";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyA-fXgytx05E72388uKC2M1L1StPtHxUzc",
+	authDomain: "fir-auth0-9b4cb.firebaseapp.com",
+	projectId: "fir-auth0-9b4cb",
+	storageBucket: "fir-auth0-9b4cb.appspot.com",
+	messagingSenderId: "531249981305",
+	appId: "1:531249981305:web:27efb67103ea51063731a8",
+	measurementId: "G-67HVFRDG0V"
+};
 
 firebase.initializeApp(firebaseConfig);
 
