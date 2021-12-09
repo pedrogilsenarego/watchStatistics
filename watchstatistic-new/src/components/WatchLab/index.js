08353118b7ea49d58fc11fb62908894c76729bce
pedrogilsenarego <pedrogilsenarego@gmail.com>
2021-12-09 10:25:36 +0000

    a

--- watchstatistic-new/src/components/WatchLab/index.js
+++ watchstatistic-new/src/components/WatchLab/index.js
@@ -12,14 +12,46 @@ const mapState = (state) => ({
 const WatchLab = () => {
 	const history = useHistory();
 	const dispatch = useDispatch();
+	const { currentUser } = useSelector(mapState);
 
 	return (
 		<div>
 			<Grid container justify="center">
 				<Paper style={{ width: "90vw", background: "#196B91" }}>
-					<Grid container>
-						<Grid item>
-							<Typography>List </Typography>
+					<Grid container style={{ padding: "20px" }}>
+						<Grid item xs={12}>
+							<Typography variant="h5">
+								Trade your points and build your watch collection
+							</Typography>
+							<Typography style={{ marginTop: "20px" }}>
+								Current Points: {currentUser.points}{" "}
+							</Typography>
+						</Grid>
+						<Grid item xs={12} style={{ marginTop: "30px" }}>
+							<Box
+								style={{
+									border: "solid 2px",
+									borderRadius: "4px",
+									borderColor: "#ffffff66",
+									background: "grey",
+									padding: "10px"
+								}}
+							>
+								<Grid container>
+									<Grid item xs={3} style={{ textAlign: "center" }}>
+										<Typography>Hello</Typography>
+									</Grid>
+									<Grid item xs={3} style={{ textAlign: "center" }}>
+										<Typography>Hello</Typography>
+									</Grid>
+									<Grid item xs={3} style={{ textAlign: "center" }}>
+										<Typography>Hello</Typography>
+									</Grid>
+									<Grid item xs={3} style={{ textAlign: "center" }}>
+										<Typography>Hello</Typography>
+									</Grid>
+								</Grid>
+							</Box>
 						</Grid>
 					</Grid>
 				</Paper>
