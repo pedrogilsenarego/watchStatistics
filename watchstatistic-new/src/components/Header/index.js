a337060ff42f637d25e6db8fc94c4fbde18a6430
pedrogilsenarego <pedrogilsenarego@gmail.com>
2022-01-20 13:59:07 +0000

    a

--- watchstatistic-new/src/components/Header/index.js
+++ watchstatistic-new/src/components/Header/index.js
@@ -56,6 +56,7 @@ const useStyles = makeStyles((theme) => ({
 			backgroundColor: "#040406BF",
 			color: "#ffffff",
 			disableScrollLock: true,
+			minWidth: "350px",
 
 			[theme.breakpoints.up(750)]: {
 				maxWidth: "350px"
