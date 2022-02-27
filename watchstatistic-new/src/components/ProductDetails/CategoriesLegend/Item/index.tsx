1f0754e70a970a5a3d5b38071813287e53ad3887
pedrogilsenarego <pedrogilsenarego@gmail.com>
2022-02-27 13:34:32 +0000

    a

---
+++ watchstatistic-new/src/components/ProductDetails/CategoriesLegend/Item/index.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { Typography, MenuItem } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+	legend: {
+		fontSize: 20,
+		fontFamily: "MyFont",
+		textAlign: "center"
+	},
+	text: {
+		fontSize: 20,
+		fontFamily: "MyFont",
+		textAlign: "center"
+	}
+}));
+
+interface Iprops {
+	title: string;
+	message?: string;
+}
+
+const Item = ({ message, title }: Iprops) => {
+	const [open, setOpen] = useState<boolean>(false);
+	const classes = useStyles();
+
+	return (
+		<div>
+			<MenuItem
+				disableRipple
+				onClick={() => {
+					setOpen(!open);
+				}}
+			>
+				<Typography className={classes.legend}>{title}</Typography>
+			</MenuItem>
+			{message && open && (
+				<Typography className={classes.text}>{message}</Typography>
+			)}
+		</div>
+	);
+};
+
+export default Item;
---
+++ watchstatistic-new/src/components/ProductDetails/CategoriesLegend/index.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Item from "./Item";
+
+// eslint-disable-next-line
+const CategoriesLegend = () => {
+	return (
+		<div>
+			<Item
+				title="S aesthetics"
+				message="Relates to the general aspect of the watch, a very personal choice"
+			/>
+			<Item
+				title="M price/quality"
+				message="The overall relative quality compared to the price tag"
+			/>
+			<Item title="L brand" />
+			<Item title="K refinement" />
+			<Item title="R history" />
+			<Item title="Q engineering" />
+			<Item title="O x-factor" />
+		</div>
+	);
+};
+
+export default CategoriesLegend;
