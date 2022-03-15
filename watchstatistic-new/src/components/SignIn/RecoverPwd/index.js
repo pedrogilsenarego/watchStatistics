52048a024f34aabf0566c35d252e1bff6db68b08
pedro <pedro.srego.ext@jumia.com>
2022-03-15 13:57:05 +0000

    a

--- watchstatistic-new/src/components/SignIn/RecoverPwd/index.js
+++ watchstatistic-new/src/components/SignIn/RecoverPwd/index.js
@@ -1,133 +1,135 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import Button from "@mui/material/Button";
-import { TextField, useTheme } from "@material-ui/core";
+import { Grid, Container, TextField } from "@mui/material";
+import { useTheme, Button } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import Container from "@mui/material/Container";
+
 import {
-	resetPasswordStart,
-	resetUserState
+  resetPasswordStart,
+  resetUserState,
 } from "./../../../redux/User/user.actions";
 const useStyles = makeStyles((theme) => ({
-	textField: {
-		"& .MuiOutlinedInput-input": { color: "white" },
-		"& . MuiInputLabel-root": {
-			color: "#ffffffB3"
-		},
-		"& .MuiInputLabel-root": { color: "grey" },
-		"& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
-			borderColor: "#ffffff",
-			borderWidth: "2px"
-		},
-		"&:hover .MuiOutlinedInput-input": {
-			color: "black"
-		},
-		"&:hover .MuiInputLabel-root": { color: "grey" },
-		"&:hover .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
-			borderColor: "#ffffffB3"
-		},
-		"&  .MuiOutlinedInput-input": {
-			color: "black"
-		},
-		"& .MuiOutlinedInput-root.Mui-focused": {
-			color: "#ffffffB3"
-		},
-		"& .MuiInputLabel-root.Mui-focused": { color: "#ffffffB3" },
-		"& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
-			borderColor: "#ffffffB3"
-		}
-	},
-	textBtn: {
-		color: "#FFFFFF",
-		fontSize: "13px",
-		backgroundColor: "!important #00000066",
-		border: "solid 2px",
-		borderColor: "orange",
-		borderRadius: "14px",
-		"&:hover": {
-			color: "#FFA500",
-			backgroundColor: "#ffffff00"
-		},
-		"&:active": {
-			color: "#FFFFFF"
-		}
-	}
+  textField: {
+    "& .MuiOutlinedInput-input": { color: "white" },
+    "& . MuiInputLabel-root": {
+      color: "#ffffffB3",
+    },
+    "& .MuiInputLabel-root": { color: "grey" },
+    "& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
+      borderColor: "#ffffff",
+      borderWidth: "2px",
+    },
+    "&:hover .MuiOutlinedInput-input": {
+      color: "black",
+    },
+    "&:hover .MuiInputLabel-root": { color: "grey" },
+    "&:hover .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline": {
+      borderColor: "#ffffffB3",
+    },
+    "&  .MuiOutlinedInput-input": {
+      color: "black",
+    },
+    "& .MuiOutlinedInput-root.Mui-focused": {
+      color: "#ffffffB3",
+    },
+    "& .MuiInputLabel-root.Mui-focused": { color: "#ffffffB3" },
+    "& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
+      borderColor: "#ffffffB3",
+    },
+  },
+  textBtn: {
+    color: "#FFFFFF",
+    fontSize: "13px",
+    backgroundColor: "#00000066",
+    border: "solid 2px",
+    borderColor: "orange",
+    borderRadius: "14px",
+    "&:hover": {
+      color: "#FFA500",
+      backgroundColor: "#ffffff00",
+    },
+    "&:active": {
+      color: "#FFFFFF",
+    },
+  },
 }));
 const mapState = ({ user }) => ({
-	resetPasswordSuccess: user.resetPasswordSuccess,
-	userErr: user.userErr
+  resetPasswordSuccess: user.resetPasswordSuccess,
+  userErr: user.userErr,
 });
 
 const RecoverPwd = ({ handleCloseLoginMenu }) => {
-	const { resetPasswordSuccess, userErr } = useSelector(mapState);
-	const dispatch = useDispatch();
-	const classes = useStyles();
-	const [email, setEmail] = useState("");
-	const [errors, setErrors] = useState([]);
-	const theme = useTheme();
+  const { resetPasswordSuccess, userErr } = useSelector(mapState);
+  const dispatch = useDispatch();
+  const classes = useStyles();
+  const [email, setEmail] = useState("");
+  const [errors, setErrors] = useState([]);
+  const theme = useTheme();
 
-	useEffect(
-		() => {
-			if (resetPasswordSuccess) {
-				dispatch(resetUserState());
-				handleCloseLoginMenu();
-			}
-		},
-		// eslint-disable-next-line
-		[resetPasswordSuccess]
-	);
+  useEffect(
+    () => {
+      if (resetPasswordSuccess) {
+        dispatch(resetUserState());
+        handleCloseLoginMenu();
+      }
+    },
+    // eslint-disable-next-line
+    [resetPasswordSuccess]
+  );
 
-	useEffect(() => {
-		if (Array.isArray(userErr) && userErr.length > 0) {
-			setErrors(userErr);
-		}
-	}, [userErr]);
+  useEffect(() => {
+    if (Array.isArray(userErr) && userErr.length > 0) {
+      setErrors(userErr);
+    }
+  }, [userErr]);
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		dispatch(resetPasswordStart({ email }));
-	};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(resetPasswordStart({ email }));
+  };
 
-	return (
-		<div className="formWrap">
-			{errors.length > 0 && (
-				<ul>
-					{errors.map((e, index) => {
-						return <li key={index}>{e}</li>;
-					})}
-				</ul>
-			)}
-			<form onSubmit={handleSubmit}>
-				<Container
-					style={{
-						backgroundColor: theme.palette.textField.background,
-						height: "40px",
-						padding: "0px",
-						marginTop: "10px",
-						borderRadius: "4px"
-					}}
-				>
-					<TextField
-						className={classes.textField}
-						size="small"
-						name="email"
-						value={email}
-						placeholder="Email"
-						onChange={(e) => setEmail(e.target.value)}
-					/>
-				</Container>
-				<Button
-					className={classes.textBtn}
-					style={{ marginTop: "15px" }}
-					variant="contained"
-					size="small"
-					type="submit"
-				>
-					Recover Password
-				</Button>
-			</form>
-		</div>
-	);
+  return (
+    <>
+      {errors.length > 0 && (
+        <ul>
+          {errors.map((e, index) => {
+            return <li key={index}>{e}</li>;
+          })}
+        </ul>
+      )}
+      <form onSubmit={handleSubmit}>
+        <Grid container>
+          <Container
+            style={{
+              backgroundColor: theme.palette.textField.background,
+              height: "40px",
+              padding: "0px",
+              marginTop: "10px",
+              borderRadius: "4px",
+            }}
+          >
+            <TextField
+              className={classes.textField}
+              size="small"
+              name="email"
+              value={email}
+              placeholder="Email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </Container>
+          <Button
+            className={classes.textBtn}
+            style={{ marginTop: "15px" }}
+            variant="contained"
+            size="small"
+            type="submit"
+          >
+            Recover Password
+          </Button>
+        </Grid>
+      </form>
+    </>
+  );
 };
 
 export default RecoverPwd;
--- watchstatistic-new/src/components/SignIn/index.js
+++ watchstatistic-new/src/components/SignIn/index.js
@@ -11,74 +11,81 @@ import RecoverPwd from "./RecoverPwd";
 import Signup from "./Signup";
 
 const SignIn = ({ handleCloseLoginMenu }) => {
-	const theme = useTheme();
-	const [whichMenu, setWhichMenu] = useState("main");
+  const theme = useTheme();
+  const [whichMenu, setWhichMenu] = useState("main");
 
-	const configMain = {
-		handleCloseLoginMenu
-	};
+  const configMain = {
+    handleCloseLoginMenu,
+  };
 
-	return (
-		<div>
-			{whichMenu === "main" && <Main {...configMain} />}
-			{whichMenu === "recover" && <RecoverPwd {...configMain} />}
-			{whichMenu === "register" && <Signup {...configMain} />}
-			<Divider
-				style={{
-					width: "100%",
-					background: theme.palette.text.faded3,
-					marginTop: "20px"
-				}}
-			/>
-			<Grid
-				item
-				container
-				xs={12}
-				justifyContent="space-between"
-				style={{ display: "flex", marginLeft: "50px", marginRight: "50px" }}
-			>
-				<Typography
-					style={{
-						fontSize: "13px",
-						color: whichMenu === "main" ? "white" : "#ffffff80",
-						paddingTop: "5px"
-					}}
-					onClick={() => {
-						setWhichMenu("main");
-					}}
-				>
-					Login
-				</Typography>
-				<Typography
-					style={{
-						fontSize: "13px",
-						color: whichMenu === "register" ? "white" : "#ffffff80",
-						paddingTop: "5px",
-						paddingLeft: "15px"
-					}}
-					onClick={() => {
-						setWhichMenu("register");
-					}}
-				>
-					Register
-				</Typography>
-				<Typography
-					style={{
-						fontSize: "13px",
-						color: whichMenu === "recover" ? "white" : "#ffffff80",
-						paddingTop: "5px",
+  return (
+    <div>
+      {whichMenu === "main" && <Main {...configMain} />}
+      {whichMenu === "recover" && <RecoverPwd {...configMain} />}
+      {whichMenu === "register" && <Signup {...configMain} />}
+      <Divider
+        style={{
+          width: "100%",
+          background: theme.palette.text.faded3,
+          marginTop: "20px",
+        }}
+      />
+      <Grid
+        item
+        container
+        spacing={2}
+        xs={12}
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Grid item textAlign="center">
+          <Typography
+            style={{
+              fontSize: "13px",
+              color: whichMenu === "main" ? "white" : "#ffffff80",
+              paddingTop: "5px",
+            }}
+            onClick={() => {
+              setWhichMenu("main");
+            }}
+          >
+            Login
+          </Typography>
+        </Grid>
+        <Grid item textAlign="center">
+          <Typography
+            style={{
+              fontSize: "13px",
+              color: whichMenu === "register" ? "white" : "#ffffff80",
+              paddingTop: "5px",
+              paddingLeft: "15px",
+            }}
+            onClick={() => {
+              setWhichMenu("register");
+            }}
+          >
+            Register
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Typography
+            style={{
+              fontSize: "13px",
+              color: whichMenu === "recover" ? "white" : "#ffffff80",
+              paddingTop: "5px",
 
-						paddingLeft: "15px"
-					}}
-					onClick={() => {
-						setWhichMenu("recover");
-					}}
-				>
-					Reset Password
-				</Typography>
-			</Grid>
-		</div>
-	);
+              paddingLeft: "15px",
+            }}
+            onClick={() => {
+              setWhichMenu("recover");
+            }}
+          >
+            Reset Password
+          </Typography>
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default SignIn;
