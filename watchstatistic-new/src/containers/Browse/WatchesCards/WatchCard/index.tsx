1028badcab1285bc36c17b1428cc20f60786a2eb
pedro <pedro.srego.ext@jumia.com>
2022-04-04 20:33:26 +0100

    a

--- watchstatistic-new/src/containers/Browse/WatchesCards/WatchCard/index.tsx
+++ watchstatistic-new/src/containers/Browse/WatchesCards/WatchCard/index.tsx
@@ -1,4 +1,11 @@
-import { Grid, CardMedia, Typography, Divider } from "@mui/material";
+import {
+  Grid,
+  CardMedia,
+  Typography,
+  Divider,
+  Box,
+  CircularProgress,
+} from "@mui/material";
 import * as Styled from "./styles";
 
 const WatchCard = ({ data, currentUser }: any) => {
@@ -14,6 +21,44 @@ const WatchCard = ({ data, currentUser }: any) => {
     productPriceBrackets,
   } = data;
 
+  function CircularProgressWithLabel() {
+    return (
+      <Box
+        component="div"
+        sx={{ position: "relative", display: "inline-flex" }}
+      >
+        <CircularProgress
+          variant="determinate"
+          value={avgTotal * 10}
+          size={80}
+          style={{ color: "orange" }}
+        />
+        <Box
+          component="div"
+          sx={{
+            top: 0,
+            left: 0,
+            bottom: 0,
+            right: 0,
+            position: "absolute",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Typography
+            variant="caption"
+            component="div"
+            color="white"
+            style={{ fontSize: "20px" }}
+          >
+            {avgTotal}
+          </Typography>
+        </Box>
+      </Box>
+    );
+  }
+
   return (
     <Grid item xs={12}>
       <Styled.Paper>
@@ -27,23 +72,32 @@ const WatchCard = ({ data, currentUser }: any) => {
               alt={reference}
             />
           </Grid>
-          <Grid item xs={8}>
-            <Typography>
-              {productBrand} {productName} {reference}
-            </Typography>
-            <Typography>{avgTotal}/10</Typography>
-            <Typography>{productCategory}</Typography>
-            <Typography>{productPriceBrackets}</Typography>
-            {currentUser && userVotes && userVotes.includes(documentID) && (
-              <Typography>AlreadyVoted</Typography>
-            )}
-            <Divider
-              style={{
-                width: "100%",
-                background: "white",
-                marginTop: "5px",
-              }}
-            />
+          <Grid item container xs={8}>
+            <Grid item container alignItems="center" xs={12}>
+              <Grid item xs={6}>
+                <Typography>
+                  {productBrand} {productName} {reference}
+                </Typography>
+
+                <Typography>{productCategory}</Typography>
+                <Typography>{productPriceBrackets}</Typography>
+                {currentUser && userVotes && userVotes.includes(documentID) && (
+                  <Typography>AlreadyVoted</Typography>
+                )}
+              </Grid>
+              <Grid item container xs={6} justifyContent="center">
+                <CircularProgressWithLabel />
+              </Grid>
+            </Grid>
+            <Grid item xs={12}>
+              <Divider
+                style={{
+                  width: "100%",
+                  background: "white",
+                  marginTop: "5px",
+                }}
+              />
+            </Grid>
           </Grid>
         </Grid>
       </Styled.Paper>
